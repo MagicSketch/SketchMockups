b48622a5fbd143becc5de3e37acabd354d1b0892
Mick <mick.mak@busicat.com>
2017-04-11 18:15:22 +0800

    Added generate template by name. Updated readme

--- README.md
+++ README.md
@@ -18,11 +18,18 @@ https://stedolan.github.io/jq/
 # Template generating script
 cd `_scripts`
 
+Tasks:
+
 Generate all templates:
 `./generate-template-pages.sh all`
 
 Generate only menu:
 `./generate-template-pages.sh count`
 
-Generate a specified template: (TODO)
-`./generate-template-pages.sh name xxxx`
+Generate a specified template: 
+`./generate-template-pages.sh name "{NAME}"`
+
+- Fetch and generate template page of name match {NAME} on Airtable (case sensitive)
+
+Example: 
+`./generate-template-pages.sh name "8 isolate"`
--- _scripts/generate-template-pages.sh
+++ _scripts/generate-template-pages.sh
@@ -64,7 +64,43 @@ if [ "$option" = "all" ]; then
 	done
 
 elif [ "$option" = "name" ]; then
-	echo "compile $2"
+	echo "Loading template with name \"$2\"."
+
+	queryURL="https://api.airtable.com/v0/appUM5HKj3inWajQG/Web%20Template%20Submission"
+
+	# Download result json to dump.json
+	curl "$queryURL" \
+	-G \
+	--data-urlencode "pageSize=100" \
+	--data-urlencode "view=Approved" \
+	--data-urlencode "filterByFormula=FIND(\"$2\",{NAME})!=0" \
+	-H "Authorization: Bearer keyxNf62XhQELuU9x" > dump.json
+
+	# Get template count
+	size=`jq '.records | length' dump.json`
+	i=0
+
+	# Loop template array
+	while [ $i -lt $size ]
+	do
+		# Slugify template name, to be used in permalink
+		templateSlug=`jq -r --arg i $i '.records[$i | tonumber].fields.Name' dump.json | awk '{$1=$1};1' | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' | tr A-Z a-z`
+
+		# Template json object
+	    templateJson=`jq --arg i $i --arg slug $templateSlug '{r: .records[$i | tonumber], s: $slug, host: .records[$i | tonumber].fields.URL | match("^http[s]?://([^/]*)")}' dump.json`
+
+	    # Compile template with template json, try -P without pre document space
+	    pug -o ../template -O "$templateJson" page-content-template.pug
+
+	    # Rename to slug name
+	    mv ../template/page-content-template.html "../template/$templateSlug.html"
+
+	    # Handle next template
+	    i=`expr $i + 1`
+	    count=`expr $count + 1`
+
+	    echo "T.$count: $templateSlug.html is generated!"
+	done
 fi
 
 if [ "$option" = "count" -o "$option" = "all" ];  then
